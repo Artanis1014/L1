6cde01545753fafd9194456eb57939197c67c476
Artanis <1745035571@qq.com>
2026-05-03 13:56:31 +0800

    day03 commit

---
+++ .idea/day04-change/main.js
@@ -0,0 +1,42 @@
+const books = [];
+const list = document.querySelector('#list');
+const btn = document.querySelector('#btn');
+const title_input = document.querySelector('#title');
+const author_input = document.querySelector('#author');
+const year_input = document.querySelector('#year');
+function render() {
+  list.innerHTML = '';
+  for (let i = 0; i < books.length; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <span>${books[i].title}</span>
+      <span>${books[i].author}</span>
+      <span>${books[i].year}</span>
+      <button class="del_btn">删除</button>
+    `;
+    const del_btn = li.querySelector('.del_btn');
+    del_btn.addEventListener('click', function(e) {
+      e.stopPropagation();
+      books.splice(i, 1);
+      render();
+    });
+    li.addEventListener('click',function(e){
+      if (e.target.classList.contains('del_btn')) return;
+      li.classList.toggle('completed')
+    })
+    list.appendChild(li);
+  }
+}
+btn.addEventListener('click', function() {
+  const title = title_input.value;
+  const author = author_input.value;
+  const year = year_input.value;
+  if (title && author && year) {
+    books.push({ title, author, year });
+    render();
+    title_input.value = '';
+    author_input.value = '';
+    year_input.value = '';
+  }
+});
+render();
